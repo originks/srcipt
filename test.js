f9f430d3a39b7ccd3bd3ef70656c96311dc49b23
刘康胜 <liukangsheng.ks@bytedance.com>
2025-05-13 19:52:13 +0800

    feat: test

---
+++ test.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Hello World KS
+// @namespace    http://tampermonkey.net/
+// @version      2025-05-13
+// @description  try to take over the world!
+// @author       You
+// @match        https://glata-staging.bytedance.net
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  alert('alets');
+  // Your code here...
+})();
